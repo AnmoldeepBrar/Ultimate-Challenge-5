49ea18c537f5ca17f035fd3bf41eab3772fabfca
AnmoldeepBrar <anmoldeepkaur2910@gmail.com>
2024-02-21 17:00:10 -0600

    New model named category has been created

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   resources :products, only: [:index, :show]
-  resource :product_details, only: [:show]
+  get 'details/:id', to: 'details#show', as: 'details'
 end
